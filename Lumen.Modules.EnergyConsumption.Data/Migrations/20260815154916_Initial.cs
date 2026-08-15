79437adb31afa3f9059f50433f926d5af54f3102
Elanis <elanis@hotmail.com>
2026-08-15 17:49:31 +0200

    Generate pg migrations

---
+++ Lumen.Modules.EnergyConsumption.Data/Migrations/20260815154916_Initial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lumen.Modules.EnergyConsumption.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "EnergyConsumption");
+
+            migrationBuilder.CreateTable(
+                name: "EnergyConsumption",
+                schema: "EnergyConsumption",
+                columns: table => new
+                {
+                    From = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    To = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Source = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false),
+                    Value = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnergyConsumption", x => new { x.From, x.To, x.Source });
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EnergyConsumption",
+                schema: "EnergyConsumption");
+        }
+    }
+}
