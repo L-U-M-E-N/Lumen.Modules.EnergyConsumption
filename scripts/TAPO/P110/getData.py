86ced0064f71bb5db4ebdb9e979bd6efe26bc2b5
Elanis <elanis@hotmail.com>
2026-08-16 10:35:40 +0200

    P110 script improvements

--- scripts/TAPO/P110/getData.py
+++ scripts/TAPO/P110/getData.py
@@ -15,14 +15,29 @@ import requests
 import pytz
 
 config = configparser.ConfigParser()
-config.read('config.ini');
+config.read('/home/pi/P110/config.ini');
 
 midnight=(datetime.datetime.now(pytz.timezone("Europe/Paris")) \
             .replace(hour=0, minute=0, second=0, microsecond=0) \
-            .astimezone(pytz.utc)
 )
-midnightTimestamp=calendar.timegm(midnight.timetuple())
+midnightTimestamp=calendar.timegm(midnight.astimezone(pytz.utc).timetuple())
 print(midnightTimestamp)
+
+firstDayOfTheMonth=midnight.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+firstDayOfTheMonthTimestamp=calendar.timegm(firstDayOfTheMonth.astimezone(pytz.utc).timetuple())
+print(firstDayOfTheMonthTimestamp)
+
+firstDayOfPreviousTheMonth=(firstDayOfTheMonth - datetime.timedelta(days=10)).replace(day=1, hour=0, minute=0, second=0, microsecond=0)
+firstDayOfPreviousTheMonthTimestamp=calendar.timegm(firstDayOfPreviousTheMonth.astimezone(pytz.utc).timetuple())
+print(firstDayOfPreviousTheMonthTimestamp)
+
+firstDayOfTheYear=midnight.replace(month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
+firstDayOfTheYearTimestamp=calendar.timegm(firstDayOfTheYear.astimezone(pytz.utc).timetuple())
+print(firstDayOfTheYearTimestamp)
+
+firstDayOfThePreviousYear=(firstDayOfTheYear - datetime.timedelta(days=10)).replace(month=1, day=1, hour=0, minute=0, second=0, microsecond=0)
+firstDayOfThePreviousYearTimestamp=calendar.timegm(firstDayOfThePreviousYear.astimezone(pytz.utc).timetuple())
+print(firstDayOfThePreviousYearTimestamp)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -31,7 +46,23 @@ class P110B(PyP110.P110):
         return self.request("get_energy_data", { "end_timestamp": endT, "interval": intervalT, "start_timestamp": startT})
 
 def formatTimestampAsIsoDate(date):
-    return datetime.datetime.fromtimestamp(fromDate).astimezone(datetime.timezone.utc).isoformat().replace('+00:00', 'Z')
+    return datetime.datetime.fromtimestamp(date).astimezone(datetime.timezone.utc).isoformat().replace('+00:00', 'Z')
+
+def mapEntries(data, fromDate, toDate, timeStampInterval):
+    for entry in data:
+        entries.append({
+            'from': formatTimestampAsIsoDate(fromDate),
+            'to': formatTimestampAsIsoDate(fromDate + timeStampInterval),
+            'value': entry,
+            'source': ('TAPO P110 - ' + ip)
+        })
+        fromDate+=timeStampInterval
+        if fromDate >= toDate:
+            break
+
+def getEnergyEntries(fromDate, toDate, minutesInerval):
+    res = p110.getEnergyData(int(fromDate), int(toDate), int(minutesInerval))
+    mapEntries(res['data'], fromDate, toDate, minutesInerval*60) 
 
 ###################
 ips = config['DEFAULT']['Ips'].split(',')
@@ -48,29 +79,46 @@ for ip in ips:
     entries=[]
 
     # Get yesterday's hourly data
-    fromDate=midnightTimestamp-86400
-    res = p110.getEnergyData(int(fromDate), int(fromDate+3600), int(60))
-
-    for entry in res['data']:
-        entries.append({
-            'from': formatTimestampAsIsoDate(fromDate),
-            'to': formatTimestampAsIsoDate(fromDate + 3600),
-            'value': entry,
-            'source': ('TAPO P110 - ' + ip)
-        })
-        fromDate+=3600
+    getEnergyEntries(
+        midnightTimestamp-86400,
+        midnightTimestamp-86400+3600*24,
+        60
+    )
 
     # Get today's hourly data
-    fromDate=midnightTimestamp
-    res = p110.getEnergyData(int(fromDate), int(fromDate+3600), int(60))
-    for entry in res['data']:
-        entries.append({
-            'from': formatTimestampAsIsoDate(fromDate),
-            'to': formatTimestampAsIsoDate(fromDate + 3600),
-            'value': entry,
-            'source': ('TAPO P110 - ' + ip)
-        })
-        fromDate+=3600
+    getEnergyEntries(
+        midnightTimestamp,
+        midnightTimestamp+3600*24,
+        60
+    )
+
+    # Get the previous month's data
+    getEnergyEntries(
+        firstDayOfPreviousTheMonthTimestamp,
+        firstDayOfPreviousTheMonthTimestamp+3600*24*31,
+        60*24
+    )
+
+    # Get this month's data
+    getEnergyEntries(
+        firstDayOfTheMonthTimestamp,
+        firstDayOfTheMonthTimestamp+3600*24*31,
+        60*24
+    )
+
+    # Get last year's data
+    #getEnergyEntries(
+    #    firstDayOfThePreviousYearTimestamp,
+    #    firstDayOfThePreviousYearTimestamp+3600*24*365,
+    #    60*24*30
+    #)
+
+    # Get this year's data
+    #getEnergyEntries(
+    #    firstDayOfTheYearTimestamp,
+    #    firstDayOfTheYearTimestamp+3600*24*365,
+    #    60*24*30
+    #)
 
     print(entries)
 
@@ -79,3 +127,4 @@ for ip in ips:
         'X-Api-Key': config['DEFAULT']['LumenApiKey']
     })
     print(response)
+
