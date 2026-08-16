40056e19d2f997c54ae48965161fe71b0f58c45f
Elanis <elanis@hotmail.com>
2026-08-16 09:43:46 +0200

    Improve upsert and make the maximum entries limit a bit higher so we can push 48x1h, 62*1d, 24x1month data from the smart plug

--- Lumen.Modules.EnergyConsumption.Module/Controllers/EnergyController.cs
+++ Lumen.Modules.EnergyConsumption.Module/Controllers/EnergyController.cs
@@ -2,6 +2,7 @@
 using Lumen.Modules.EnergyConsumption.Data;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Lumen.Modules.EnergyConsumption.Module.Controllers {
     [ApiController]
@@ -10,7 +11,7 @@ namespace Lumen.Modules.EnergyConsumption.Module.Controllers {
 
         [HttpPost]
         public async Task<IActionResult> SubmitEtnries([FromBody] IEnumerable<EnergyConsumptionPointInTime> entries, CancellationToken cancellationToken = default) {
-            if (entries.Count() > 100) {
+            if (entries.Count() > 150) {
                 return BadRequest();
             }
 
@@ -26,11 +27,14 @@ namespace Lumen.Modules.EnergyConsumption.Module.Controllers {
 
         private async Task UpserEntries(IEnumerable<EnergyConsumptionPointInTime> entries, CancellationToken cancellationToken) {
             foreach (var newEntry in entries) {
-                if (context.EnergyConsumption.Any((x) => x.From == newEntry.From && x.To == newEntry.To && x.Source == newEntry.Source)) {
-                    logger.LogInformation("{Module} - Entry '{From}' '{To}' '{Source}' already existing, skipping", nameof(EnergyConsumptionModule), newEntry.From, newEntry.To, newEntry.Source);
+                var entry = await context.EnergyConsumption.FirstOrDefaultAsync((x) => x.From == newEntry.From && x.To == newEntry.To && x.Source == newEntry.Source, cancellationToken);
+                if (entry is not null) {
+                    logger.LogInformation("{Module} - Entry '{From}' '{To}' '{Source}' already existing, updating", nameof(EnergyConsumptionModule), newEntry.From, newEntry.To, newEntry.Source);
+                    entry.Value = newEntry.Value;
                     continue;
                 }
 
+                logger.LogInformation("{Module} - Entry '{From}' '{To}' '{Source}' not already existing, adding", nameof(EnergyConsumptionModule), newEntry.From, newEntry.To, newEntry.Source);
                 await context.EnergyConsumption.AddAsync(newEntry, cancellationToken);
             }
 
